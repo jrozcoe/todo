78b3b5ee831b8914a850f5e52e9a5eb6c294cd7d
Jordan Roszkowiak <jordanroszkowiak@gmail.com>
2020-01-21 15:04:58 +0000

    final commit

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,16 +1,38 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the TasksHelper. For example:
-#
-# describe TasksHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
+RSpec.describe TasksController, type: :controller do
+  describe "tasks#index" do
+    it "should list the tasks in the database" do
+      task1 = FactoryBot.create(:task)
+      task2 = FactoryBot.create(:task)
+      task1.update_attributes(title: "Something else")
+      get :index
+      expect(response).to have_http_status :success
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value.count).to eq(2)
+      response_ids = response_value.collect do |task|
+        task["id"]
+      end
+      expect(response_ids).to eq([task1.id, task2.id])
+    end
+  end
+  describe "tasks#update" do
+    it "should allow tasks to be marked as done" do
+      task = FactoryBot.create(:task, done: false)
+      put :update, params: {id: task.id, task: { done: true }}
+      expect(response).to have_http_status(:success)
+      task.reload
+      expect(task.done).to eq(true)
+    end
+  end
 
-RSpec.describe TasksHelper, type: :helper do
-
+  describe "tasks#create" do
+    it "should allow new tasks to be created" do
+      post :create, params: {task: {title: "Fix things"}}
+      expect(response).to have_http_status(:success)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value['title']).to eq("Fix things")
+      expect(Task.last.title).to eq("Fix things")
+    end
+  end
 end
